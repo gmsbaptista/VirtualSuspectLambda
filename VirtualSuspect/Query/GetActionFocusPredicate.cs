047aaf48c1d63c5197104c0a3f46cf4691d72d34
Gonçalo Baptista <goncalo.baptista@tecnico.ulisboa.pt>
2020-01-06 16:18:29 +0000

    Added GetActionFocusPredicate class

---
+++ VirtualSuspect/Query/GetActionFocusPredicate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualSuspect.KnowledgeBase;
+
+namespace VirtualSuspect.Query
+{
+    public class GetActionFocusPredicate : IFocusPredicate
+    {
+        public Func<EventNode, QueryResult.Result> CreateFunction()
+        {
+            return delegate (EventNode node) {
+
+                return new QueryResult.Result(node.Action, 1, KnowledgeBaseManager.DimentionsEnum.Action);
+
+            };
+        }
+
+        public KnowledgeBaseManager.DimentionsEnum GetSemanticRole()
+        {
+            return KnowledgeBaseManager.DimentionsEnum.Action;
+        }
+    }
+}
